3f8b3211f86b2fb2b4ebb2ea6ecbde2507d4ed4f
irvall <johanirvall@gmail.com>
2018-03-07 18:59:41 +0100

    Update Rowperator.java

--- Rowperator.java
+++ Rowperator.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
+import java.text.DecimalFormat;
 
 class Rowperator {
 
-    float[][] matrix;
+    double[][] matrix;
 
     public Rowperator(String input) {
         matrix = parse(input);
@@ -13,7 +14,7 @@ class Rowperator {
 
     public Rowperator(int row, int col) {
         Random r = new Random();
-        matrix = new float[row][col];
+        matrix = new double[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 matrix[i][j] = r.nextInt(20) + 1;
@@ -26,7 +27,7 @@ class Rowperator {
         Random r = new Random();
         int x = r.nextInt(10) + 1;
         int y = r.nextInt(10) + 1;
-        matrix = new float[x][y];
+        matrix = new double[x][y];
         System.out.printf("Pseudo-randomly generated a %d x %d matrix\n", x, y);
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
@@ -59,15 +60,15 @@ class Rowperator {
         return out;
     }
 
-    private float[][] parse(String input) {
+    private double[][] parse(String input) {
         String[] sa = input.split(" ");
         int row = Integer.parseInt(sa[0]);
         int col = Integer.parseInt(sa[1]);
-        float[][] out = new float[row][col];
+        double[][] out = new double[row][col];
         int k = 2;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                out[i][j] = Float.parseFloat(sa[k++]);
+                out[i][j] = Double.parseDouble(sa[k++]);
             }
         }
         return out;
@@ -99,7 +100,7 @@ class Rowperator {
 
     private void swapRow(int r1, int r2) {
         for (int i = 0; i < matrix[0].length; i++) {
-            float t = matrix[r1][i];
+            double t = matrix[r1][i];
             matrix[r1][i] = matrix[r2][i];
             matrix[r2][i] = t;
         }
@@ -179,7 +180,8 @@ class Rowperator {
     private void show() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + "\t");
+                DecimalFormat numberFormat = new DecimalFormat("#.00");
+                System.out.print(numberFormat.format(matrix[i][j]) + "\t");
             }
             System.out.println();
         }
